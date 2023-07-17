b55761dadb3ff481b8f1e56b1f538600bd942e96
husanbek123 <husanbek555@gmail.com>
2023-07-17 11:17:57 +0500

    QuranApi v1

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -3,12 +3,21 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import surahsData from "@/data/surahs.json";
 
 type Data = {
-  video: string;
+  name_uz: string;
+  video_uz: string;
+  meaning_uz: string[];
+  numberOfAyahs: number;
 };
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data[]>
+  res: NextApiResponse<Data[] | Data>
 ) {
+  if (req.query.surah) {
+    const requestedData = surahsData.find((surah) =>
+      surah.name_uz.startsWith(req.query.surah as string)
+    );
+    res.status(200).json(requestedData as Data);
+  }
   res.status(200).json(surahsData);
 }
